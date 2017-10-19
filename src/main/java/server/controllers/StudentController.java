0d3e905d1892ca7e0f078b79ace4a04065c6853a
Mathias Täck <MathiasTack@mathiass-mbp-2.hhk.dk>
2017-10-19 21:34:00 +0200

    kode comments

--- src/main/java/server/controllers/StudentController.java
+++ src/main/java/server/controllers/StudentController.java
@@ -17,41 +17,50 @@ public class StudentController {
         return attendingEvents;
     }
 
-/*
-    public StudentController() {
-        this.gson = new Gson();
+    /*
+        public StudentController() {
+            this.gson = new Gson();
 
-    }
+        }
 
-    public ArrayList<Student> getStudents() {
-        DBmanager db = new DBmanager();
-        ArrayList<Student> students = db.getStudents();
-        return students;
-    }
+        public ArrayList<Student> getStudents() {
+            DBmanager db = new DBmanager();
+            ArrayList<Student> students = db.getStudents();
+            return students;
+        }
 
-/*
-    public Student getStudents(int id) {
-        DBmanager db = new DBmanager();
-        Student student = db.getStudents(id);
-        return students;
-    }*/
+    /*
+        public Student getStudents(int id) {
+            DBmanager db = new DBmanager();
+            Student student = db.getStudents(id);
+            return students;
+        }*/
 
+//Denne metode bruges til at verificere en student, når brugeren skal oprette sig i døk social.
     public Student verifyStudentCreation(String firstName, String lastName, String password, String email) throws IllegalArgumentException {
         Student verifiedStudent = new Student(firstName, lastName, password, email);
 
-        //nedenunder tjekkes der for diverse krav.
-        // nedenunder viser at der skal være et "@".
+            // Nedenunder viser at der skal være et "@", når man indtaster sin mail.
         if (!email.contains("@")) {
             throw new IllegalArgumentException("Email did not contain @");
-            // Dette gør at serveren kræver at der ikke indgår tal i navn.
+
+            // Dette gør at serveren kræver at der ikke indgår tal i navnet. og at det indtastet navn indeholder mindst 2 char.
+            // ".*\\d.*" --> Tjekker om der er tal i navnet.
         } else if (firstName.length() < 2 || firstName.matches(".*\\d.*")) {
             throw new IllegalArgumentException("firstName must contain at least 2 characters, and can't contain numbers");
+
+
+            // Dette tjekker det samme som overstående kode, bare med efternavn.
         } else if (lastName.length() < 2 || lastName.matches(".*\\d.*")) {
             throw new IllegalArgumentException("lastName must contain at least 2 characters, and can't contain numbers");
 
-            // her tjekkes om det indtastede indeholder mindst et bogstaver samt mindst et tal.
+
+            // her tjekkes om det indtastede indeholder mindst et bogstav samt mindst et tal.
+            // ".*[a-zA->].*" --> Her tjekker koden om det indtastet kode indeholder enten store og små bogstaver.
+            // tæller dog ikke æ,ø og å.
         } else if (!password.matches(".*[a-zA-Z].*") || !password.matches(".*\\d.*")) {
             throw new IllegalArgumentException("password must contain atleast one char and one number");
+
         }
         //hvis alle tjeks godkendes så bliver studenten returneret
         return verifiedStudent;
